e0fe94568d3fc9780159cf492f24dfa4c2017815
mami <kbb33m0730@gmail.com>
2026-05-19 12:29:35 +0900

    seedレシピ修正

--- src/database/seeders/AllergyRecipeTableSeeder.php
+++ src/database/seeders/AllergyRecipeTableSeeder.php
@@ -21,31 +21,131 @@ class AllergyRecipeTableSeeder extends Seeder
             ],
             [
                 'recipe_id' => 1,
+                'allergy_id' => 7,
+            ],
+            [
+                'recipe_id' => 2,
+                'allergy_id' => 2,
+            ],
+            [
+                'recipe_id' => 2,
+                'allergy_id' => 3,
+            ],
+            [
+                'recipe_id' => 2,
                 'allergy_id' => 4,
             ],
             [
-                'recipe_id' => 1,
-                'allergy_id' => 18,
+                'recipe_id' => 2,
+                'allergy_id' => 9,
             ],
             [
-                'recipe_id' => 1,
+                'recipe_id' => 2,
+                'allergy_id' => 10,
+            ],
+            [
+                'recipe_id' => 2,
+                'allergy_id' => 15,
+            ],
+            [
+                'recipe_id' => 2,
+                'allergy_id' => 25,
+            ],
+            [
+                'recipe_id' => 3,
+                'allergy_id' => 2,
+            ],
+            [
+                'recipe_id' => 4,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 4,
+                'allergy_id' => 17,
+            ],
+            [
+                'recipe_id' => 4,
                 'allergy_id' => 21,
             ],
             [
-                'recipe_id' => 1,
-                'allergy_id' => 22,
-            ],
-            [
-                'recipe_id' => 1,
-                'allergy_id' => 24,
-            ],
-            [
-                'recipe_id' => 2,
+                'recipe_id' => 5,
                 'allergy_id' => 4,
             ],
             [
-                'recipe_id' => 2,
+                'recipe_id' => 5,
+                'allergy_id' => 18,
+            ],
+            [
+                'recipe_id' => 5,
+                'allergy_id' => 24,
+            ],
+            [
+                'recipe_id' => 6,
+                'allergy_id' => 2,
+            ],
+            [
+                'recipe_id' => 6,
+                'allergy_id' => 3,
+            ],
+            [
+                'recipe_id' => 6,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 6,
+                'allergy_id' => 24,
+            ],
+            [
+                'recipe_id' => 7,
+                'allergy_id' => 2,
+            ],
+            [
+                'recipe_id' => 7,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 7,
+                'allergy_id' => 21,
+            ],
+            [
+                'recipe_id' => 7,
+                'allergy_id' => 24,
+            ],
+            [
+                'recipe_id' => 8,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 8,
+                'allergy_id' => 21,
+            ],
+            [
+                'recipe_id' => 8,
+                'allergy_id' => 29,
+            ],
+            [
+                'recipe_id' => 9,
+                'allergy_id' => 2,
+            ],
+            [
+                'recipe_id' => 9,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 9,
+                'allergy_id' => 21,
+            ],
+            [
+                'recipe_id' => 9,
                 'allergy_id' => 22,
+            ],
+            [
+                'recipe_id' => 10,
+                'allergy_id' => 4,
+            ],
+            [
+                'recipe_id' => 10,
+                'allergy_id' => 21,
             ],
         ]);
     }
--- src/database/seeders/RecipesTableSeeder.php
+++ src/database/seeders/RecipesTableSeeder.php
@@ -21,8 +21,16 @@ class RecipesTableSeeder extends Seeder
         $disk->makeDirectory('images');
 
         $images = [
-            'curry.png',
-            'karaage.png',
+            'ajillo.jpg',
+            'browny.jpg',
+            'cake.jpg',
+            'gyudon.jpg',
+            'hoikoro.jpg',
+            'hotsand.jpg',
+            'korokke.jpg',
+            'nimono.jpg',
+            'oyakodon.jpg',
+            'pumpkin.jpg',
         ];
 
         foreach ($images as $image) {
@@ -37,19 +45,83 @@ class RecipesTableSeeder extends Seeder
         DB::table('recipes')->insert([
             [
                 'user_id' => 1,
-                'name' => 'カレーライス',
-                'image' => 'images/curry.png',
-                'description' => '子どもに大人気のカレーです',
-                'servings' => 4,
-                'tips' => 'はちみつを入れることでまろやかさが出ます。'
+                'name' => 'エビのアヒージョ',
+                'image' => 'images/ajillo.jpg',
+                'description' => 'おいしいスペイン料理のアヒージョです。',
+                'servings' => '2人分',
+                'tips' => 'お皿の大きさに合わせてオイルの量を調節してください。'
             ],
             [
                 'user_id' => 1,
-                'name' => '塩麹から揚げ',
-                'image' => 'images/karaage.png',
-                'description' => '鶏むね肉でも柔らかい唐揚げです。',
-                'servings' => 2,
-                'tips' => '鶏肉を塩麹に漬けることでお肉が柔らかくなります。'
+                'name' => 'チョコブラウニー',
+                'image' => 'images/browny.jpg',
+                'description' => '甘くておいしいブラウニーです。',
+                'servings' => '12×12cm型 1枚分',
+                'tips' => '甘さは砂糖の量で調節してください。'
+            ],
+            [
+                'user_id' => 2,
+                'name' => '幸せのショートケーキ',
+                'image' => 'images/cake.jpg',
+                'description' => '乳アレルギーの息子のために豆乳を使用して作りました。',
+                'servings' => '直径18cm型',
+                'tips' => 'クリームはレモン果汁を入れることで、さっぱりし溶けにくくなります。'
+            ],
+            [
+                'user_id' => 2,
+                'name' => '牛丼',
+                'image' => 'images/gyudon.jpg',
+                'description' => '子どもも大好き牛丼',
+                'servings' => '1人前',
+                'tips' => '煮詰めすぎると味が濃くなるのでお好みで調整してください。'
+            ],
+            [
+                'user_id' => 1,
+                'name' => '素いらず！本格回鍋肉',
+                'image' => 'images/hoikoro.jpg',
+                'description' => 'おうちにあるものだけで簡単に作れる本格回鍋肉です',
+                'servings' => '4人前',
+                'tips' => '生姜を入れるのがポイントです。'
+            ],
+            [
+                'user_id' => 1,
+                'name' => 'アレンジいろいろホットサンド',
+                'image' => 'images/hotsand.jpg',
+                'description' => '朝ごはんを手抜きしたいときによく作ります',
+                'servings' => '1人前',
+                'tips' => '今回はトマトとレタスで作りましたが、ほかの具も挑戦してみてください。'
+            ],
+            [
+                'user_id' => 2,
+                'name' => 'ほくほくコロッケ',
+                'image' => 'images/korokke.jpg',
+                'description' => '少ない油で簡単コロッケ',
+                'servings' => '4個分',
+                'tips' => 'レンジを使うことで時短になります。'
+            ],
+            [
+                'user_id' => 1,
+                'name' => '味しみしみ煮物',
+                'image' => 'images/nimono.jpg',
+                'description' => '懐かしいホッとする味の煮物です。',
+                'servings' => '2人前',
+                'tips' => '冷ますときに具材に味がよくしみ込みます'
+            ],
+            [
+                'user_id' => 2,
+                'name' => '簡単親子丼',
+                'image' => 'images/oyakodon.jpg',
+                'description' => '簡単に済ませたい休日によく作ります。',
+                'servings' => '2人前',
+                'tips' => 'にんじんやシイタケを入れることで栄養価がUPします。'
+            ],
+            [
+                'user_id' => 1,
+                'name' => '時短かぼちゃの煮つけ',
+                'image' => 'images/pumpkin.jpg',
+                'description' => '忙しい朝のお弁当作りの救世主です。',
+                'servings' => '2人前～',
+                'tips' => '固い場合は追加でレンジで温めてください。'
             ],
         ]);
     }
